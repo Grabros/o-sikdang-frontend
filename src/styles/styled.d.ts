1ab07e85c52c5ada49f98f54fc39585d604fea1c
choijangho <jhchoi1115@gmail.com>
2020-09-19 21:19:52 +0900

    Style : DefaultTheme declare 수정

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,11 +3,8 @@ import "styled-components";
 // and extend them!
 declare module "styled-components" {
   export interface DefaultTheme {
-    colors: {
-      basicBg: string;
-      basicFont: string;
-
-      yellow_1: string;
-    };
+    basicBg: string;
+    basicFont: string;
+    yellow: string;
   }
 }
