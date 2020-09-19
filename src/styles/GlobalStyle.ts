b409f45a2e95b8ab57534067ce14b6d9d25f2a0f
choijangho <jhchoi1115@gmail.com>
2020-09-19 21:20:31 +0900

    Style : GlobalStyle 속성 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -10,8 +10,8 @@ export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    font-size : 10px;
-    color : ${(props) => props.theme.colors.basicFont};
+    color : ${(props) => props.theme.basicFont};
+    background-color : ${(props) => props.theme.basicBg};
     box-sizing: border-box;
   }
 
@@ -25,8 +25,42 @@ export const GlobalStyle = createGlobalStyle`
       @media (min-width: 768px) {
         font-size: ${px2vw(18)};
       }
-
     }
+
+    button {
+    background: inherit;
+    border: none;
+    box-shadow: none;
+    border-radius: 0;
+    padding: 0;
+    overflow: visible;
+    cursor: pointer;
+    outline : none;
+  }
+
+  h1 {
+    font-size : 3rem;
+
+    @media (min-width : 1024px) {
+      font-size : 2.5rem;
+    }
+  }
+
+  h2 {
+    font-size : 2.5rem;
+
+    @media (min-width : 1024px) {
+      font-size : 2rem;
+    }
+  }
+  
+  h3 {
+    font-size : 2rem;
+
+    @media (min-width : 1024px) {
+      font-size : 1.5rem;
+    }
+  }
 `;
 
 export default GlobalStyle;
